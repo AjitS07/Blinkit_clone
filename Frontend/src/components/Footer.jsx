da6f4ebe4fc0de434e79b951f6982b972a5ae0f5
Ajit sharma <ajitsharma1006@gmail.com>
2026-01-11 22:51:42 +0530

    Add Frontend Folder

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <footer className='border-t'>
+    <div className='container mx-auto p-4'>
+
+    <p>All Rights Reserved By Ajit Sharma - 2025 ~</p>
+    </div>
+     
+   </footer>
+  )
+}
+
+export default Footer
